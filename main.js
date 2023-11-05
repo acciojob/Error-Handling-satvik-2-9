af12188032cf9b56c45b25bfa89e1339d9b1e20e
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 16:23:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
 const fs = require('fs');
-const csv = require('csv-parser');
 
-const csvFilePath = process.argv[2];
+// Check if the right number of arguments are passed
+if (process.argv.length !== 4) {
+  console.error('Usage: node average-csv.js <path-to-csv> <column-name>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
 const columnName = process.argv[3];
 
-let sum = 0;
-let count = 0;
+// Read the CSV file
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  }
 
-fs.createReadStream(csvFilePath)
-  .pipe(csv())
-  .on('data', (data) => {
-    const value = Number(data[columnName]);
+  const lines = data.trim().split('\n');
+  const header = lines[0].split(',');
+
+  // Check if the column name exists
+  const columnIndex = header.indexOf(columnName);
+  if (columnIndex === -1) {
+    console.log(`No valid values found in the ${columnName} column.`);
+    process.exit(0);
+  }
+
+  let sum = 0;
+  let count = 0;
+
+  // Calculate the average
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(',');
+    const value = parseFloat(values[columnIndex]);
+
     if (!isNaN(value)) {
       sum += value;
       count++;
     }
-  })
-  .on('end', () => {
-    if (count > 0) {
-      const average = sum / count;
-      console.log(`The average value of ${columnName} is: ${average}`);
-    } else {
-      console.log(`No valid values found in the ${columnName} column.`);
-    }
-  })
-  .on('error', (err) => {
-    console.error(`Error reading CSV file: ${err}`);
-  });
+  }
+
+  if (count === 0) {
+    console.log(`No valid values found in the ${columnName} column.`);
+    process.exit(0);
+  }
+
+  const average = sum / count;
+  console.log(`The average value of ${columnName} is: ${average}`);
+});
